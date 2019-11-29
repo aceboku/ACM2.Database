1337e6977e8127edbab72a55e6bbdc32ad8892ae
amartinez5 <a.martinez@pharmahealth.us>
2019-11-28 19:55:51 -0500

    Add project files.

---
+++ dbo/Stored Procedures/CustomerRetrieve.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[CustomerRetrieve]
+	@StartsWith AS VARCHAR (1)
+AS
+BEGIN
+	SELECT Customer.CustomerId, 
+		Customer.LastName,
+		Customer.FirstName,
+		Customer.EmailAddress
+	FROM Customer
+	WHERE LastName LIKE @StartsWith + '%'
+	ORDER BY LastName
+END
---
+++ dbo/Tables/Customer.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Customer] (
+    [CustomerId]   INT           IDENTITY (1, 1) NOT NULL,
+    [LastName]     NVARCHAR (50) NULL,
+    [FirstName]    NVARCHAR (50) NULL,
+    [EmailAddress] NVARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([CustomerId] ASC)
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Customer_LastName]
+    ON [dbo].[Customer]([LastName] ASC);
+
